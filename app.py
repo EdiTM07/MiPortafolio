798ae97f920b5ef40be6419c5e26f5f7ede1ebd1
Edison Tituaña <editm1997@gmail.com>
2025-10-02 19:13:03 -0500

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, template_folder=".")
+
+# Rutas para servir tus carpetas estáticas
+@app.route('/css/<path:filename>')
+def css(filename):
+    return send_from_directory('css', filename)
+
+@app.route('/js/<path:filename>')
+def js(filename):
+    return send_from_directory('js', filename)
+
+@app.route('/img/<path:filename>')
+def img(filename):
+    return send_from_directory('img', filename)
 
 @app.route("/")
 def home():
@@ -9,4 +21,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
