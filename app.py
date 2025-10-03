fcb18fe99b90948417351e05fe63c1a520f8bd78
Edison Tituaña <editm1997@gmail.com>
2025-10-02 19:06:45 -0500

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+
+
+app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def home():
@@ -7,3 +9,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
